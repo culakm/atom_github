063e722fcaef6e6683eb2fe8821c8df7a36ad18c
culakm <culak.martin@gmail.com>
2020-04-03 15:01:34 +0200

    komentovany subor

--- fifove_cisla.pl
+++ fifove_cisla.pl
@@ -6,17 +6,30 @@ use warnings;
 use Data::Dumper;
 
 
+#https://www.hackmath.net/cz/kalkulacka/kombinace-a-permutace?n=10&k=4&order=1&repeat=0
+
+my $graphic = 0;
+# String obsahujuci cislo, pocet cisel je pocet prvkov
 my $string_orig = 79245503;
 my @array_orig = split //,$string_orig;
 my $max_cislo = 0;
-my $min_cislo = 99999999999999999990;
+my $min_cislo = 0;
 
-my $n = scalar(@array_orig); #pocet prvkov
-my $k = 3; #pocet vyberov
+# Pocet prvkov
+my $n = scalar(@array_orig);
+# Maximalny index pre poziciu v poli prvkov
+my $Imax = $n-1;
+# Pocet vyberov
+my $k = 3;
+# pocet kombinacii bez opakovania a bez poradia
 my $c = fact($n)/(fact($k)*(fact($n-$k)));
 printf "Pocet kombinacii: %s\n",$c;
-my $Imax = $n-1;
 
+
+# priprava pola pozicii pre vybery
+# kazdy vyber ma
+# max_miesto = maximalny index na ktorom sa moze vyskytnut
+# miesto = index na ktorom sa momentalne nachadza
 my $A = [];
 for (my $i = 0; $i < $k; $i++){
 	my $max_miesto = $Imax - ($k - ($i+1));
@@ -27,27 +40,43 @@ for (my $i = 0; $i < $k; $i++){
 	};
 }
 
+# Prechadzame pocet moznych kombinacii
 for ( my $i = 1; $i <= $c; $i++){
-	#print_array($A,$n);
+
+	# vytlaci umiestnenie prvkov v aktualnej kombinacii
+	print_array($A,$n) if $graphic;
+	# Vytvorenie testovacieho pola v ktorom budu podla pola pozicii nastavene niektore prvky ako undef
 	my @a2test = @array_orig;
-	#print "orig array: ",Dumper(\@array_orig),"\n";
-	#print "new array: ",Dumper(\@a2test),"\n";
 	my $int = extract_int($A,\@a2test);
 
-	#najdi max
-	if ($int > $max_cislo){$max_cislo = $int;}
-	if ($int < $min_cislo){$min_cislo = $int;}
+	# prvy beh naloaduje max a min
+	if ($i == 1){
+		$max_cislo = $int;
+		$min_cislo = $int;
+	}
+	else {
+		#porovnaj max
+		if ($int > $max_cislo){$max_cislo = $int;}
+		#porovnaj min
+		if ($int < $min_cislo){$min_cislo = $int;}
+	}
 
+	# bol niektory vyber uz posunuty?
 	my $posun = 0;
 	for (my $j = ($k - 1); $j >= 0; $j--){
+		# uz sa niektory vyber predtym v tejto kombinacii pohol
 		if ($posun){
 			last;
 		}
 		if ($A->[$j]{'miesto'} < $A->[$j]{'max_miesto'}){
+			# posun vyber
 			$A->[$j]{'miesto'}++;
 			$posun = 1;
+			# existuje nejaky vyber viac v pravo ako je tento vyber?
 			if (exists $A->[$j+1]){
+				# nastav najblizsie nove miesto
 				my $nove_miesto = $A->[$j]{'miesto'} + 1;
+				# Pre vsetky vybery ktore su viac v pravo ako je tento vyber uprav miesto tak aby bolo co najblizsie ku mne
 				for(my $m = $j+1; $m < $k; $m++){
 					$A->[$m]{'miesto'} = $nove_miesto++;
 				}
@@ -59,6 +88,8 @@ for ( my $i = 1; $i <= $c; $i++){
 print "Najmensie cislo je : $min_cislo \n";
 print "Najvacsie cislo je : $max_cislo \n";
 
+# Z pola cisel $a2test zmen na undef cisla na poziciach ozacenych v $A
+# vrati int cislo jednej kombinacie
 sub extract_int{
 	my ($A,$a2test) = @_;
 	my $n = scalar(@{$a2test});
@@ -70,11 +101,10 @@ sub extract_int{
 		}
 	}
 	my $int = array2int($a2test);
-	#print_array($A,$n);
-	#print "testovane cislo: $int\n";
 	return $int;
 }
 
+# Z pola cisel vynechaj vsetky prvky ktore su undef a vytvor z toho int
 sub array2int {
 	my ($array) = @_;
 	my @array_nn = grep { defined $_ } @{$array};
@@ -82,7 +112,7 @@ sub array2int {
 	return $int;
 }
 
-
+# Podla pola $A vytvor riadok tejto kombinacie kde vybery su 0 a ostatne *
 sub print_array{
 	my ($A,$n) = @_;
 	for (my $i = 0; $i < $n ; $i++){
@@ -94,9 +124,6 @@ sub print_array{
 			if ($A_status->{'miesto'} == $i ){
 				$out = ' 0 ' ;
 			}
-			#print Dumper($A_status);
-			#print $A_status->{'miesto'},"\n";
-
 		}
 		print $out;
 	}
@@ -104,7 +131,7 @@ sub print_array{
 }
 
 
-
+# Vypocet faktorialu
 sub fact{
 	my ($n) = @_;
 	my $f = 1;
